916867ab7a953873c98e072a5eb62105fb70b4e9
chanchit <chlear@celestica.com>
2021-07-10 18:39:05 +0700

    Add clean architecture structure

---
+++ DotNet-101.Core/DTOs/ProductViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNet_101.Core.DTOs
+{
+    public class ProductViewModel : BaseDTO
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Qty { get; set; }
+    }
+}
