4ea26e615976cebb8e662718712f98ca2444a65e
chanchit <chlear@celestica.com>
2021-07-10 19:07:30 +0700

    Update generic repo files

---
+++ DotNet-101.Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNet_101.Core.Interfaces
+{
+    public interface IGenericRepository
+    {
+
+    }
+}
---
+++ DotNet-101.Infrastructure/SqlServer/GenericRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNet_101.Infrastructure.SqlServer
+{
+    public class GenericRepository
+    {
+    }
+}
