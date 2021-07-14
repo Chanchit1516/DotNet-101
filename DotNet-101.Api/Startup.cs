8336567b3323215f4cab720d3f6687d02302061c
chanchit <chlear@celestica.com>
2021-07-14 18:18:57 +0700

    Add swagger and remove default controller

--- DotNet-101.Api/Startup.cs
+++ DotNet-101.Api/Startup.cs
@@ -9,6 +9,7 @@ using DotNet_101.Infrastructure.Data;
 using DotNet_101.Infrastructure.IoC;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -16,6 +17,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace DotNet_101.Api
 {
@@ -35,6 +37,17 @@ namespace DotNet_101.Api
             {
                 option.UseSqlServer(@"Data Source=DESKTOP-URTA846\SQLEXPRESS;Database=developmentDb;User ID=sa;Password=123456;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
             });
+
+            #region Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "EFCore.CodeFirst.WebApi",
+                });
+            });
+            #endregion
 
             services.AddControllers();
 
@@ -67,8 +80,19 @@ namespace DotNet_101.Api
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
             });
+
+            #region Swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "EFCore.CodeFirst.WebApi");
+            });
+            #endregion
         }
     }
 }
