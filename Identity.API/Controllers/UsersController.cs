c0e18077057050ec1d4810e8db1d9f1168dc2289
chanchit <chlear@celestica.com>
2021-07-19 21:27:51 +0700

    Created identity project

---
+++ Identity.API/Controllers/UsersController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Identity.API.Interface;
+using Identity.API.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private IUserService _userService;
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate(AuthenticateRequest model)
+        {
+            var response = _userService.Authenticate(model);
+            
+            if (response == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            return Ok(response);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var users = _userService.GetAll();
+            return Ok(users);
+        }
+
+    }
+}
---
+++ Identity.API/Interface/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using Identity.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identity.API.Interface
+{
+    public interface IUserService
+    {
+        AuthenticateResponse Authenticate(AuthenticateRequest model);
+        IEnumerable<User> GetAll();
+        User GetById(int id);
+    }
+}
